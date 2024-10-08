ed5cb99adf5cedd6881779f2783f7dcd862ed86d
andrew-Go1 <deletecrack@gmail.com>
2024-10-08 23:25:00 +0300

    feat: add entry piont

---
+++ index.js
@@ -0,0 +1,12 @@
+const calculator = require('./src/calculator');
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Введите выражение: ", expression => {
+  console.log("Результат:", calculator(expression));
+  rl.close();
+});
